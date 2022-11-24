dee8d7d388abc3dcf3f36d3b7f28349c7557daff
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-24 11:52:49 +0600

    swiperjs installed and added banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import img1 from "../../../Images/img1.jpg";
+import img2 from "../../../Images/img2.jpg";
+// import Swiper core and required modules
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  EffectFade,
+} from "swiper";
+
+import { Swiper, SwiperSlide} from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+import "swiper/css/effect-fade";
+
 
 const Banner = () => {
-    return (
-        <div>
-            <h2>banner section</h2>
-        </div>
-    );
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      effect={"fade"}
+      autoplay
+      loop
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="max-h-screen"
+    >
+      <SwiperSlide>
+        <img src={img1} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img2} className="w-full" alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default Banner;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 const Navbar = () => {
 
-    const menu = <>
-        
-    </>
+    const menu = (
+      <>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Categories</a>
+        </li>
+        <li>
+          <a>Blogs</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+      </>
+    );
     return (
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-blue-900">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -29,72 +42,18 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="btn btn-ghost normal-case text-xl">ResalePort.com</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menu}
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <a className="btn">Sign In</a>
         </div>
       </div>
     );
