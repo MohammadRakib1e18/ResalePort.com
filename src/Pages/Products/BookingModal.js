01b149d32e417ac9579bd822238597a8dbffca27
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-27 20:46:56 +0600

    changed background color with the pop-up of the modal

--- src/Pages/Products/BookingModal.js
+++ src/Pages/Products/BookingModal.js
@@ -12,26 +12,30 @@ const BookingModal = ({show, setShow, productInfo}) => {
             <input
               type="checkbox"
               id="purchase-modal"
-              className="modal-toggle"
+              className="modal-toggle "
             />
-            <div className="modal">
-              <div className="modal-box relative">
+            <div className="modal bg-gray-900/90">
+              <div className="modal-box relative text-slate-200 bg-slate-700">
                 <label
                   htmlFor="purchase-modal"
                   className="btn btn-sm btn-circle absolute right-2 top-2"
                 >
                   ✕
                 </label>
-                <h3 className="text-lg font-bold">{productInfo?.title}</h3>
+                <h3 className="text-3xl font-bold text-blue-400">
+                  {productInfo?.title}
+                </h3>
+                <div className="divider"></div>
+
                 <p className="py-4">
                   You've been selected for a chance to get one year of
                   subscription to use Wikipedia for free!
                 </p>
+
                 <div className="w-full">
                   <label
                     onClick={() => {
                       setShow(false);
-                      // setCloseModal(false);
                     }}
                     className="w-full mt-5 btn bg-gradient-to-r from-cyan-500 to-blue-500 rounded-3xl border-none text-slate-100"
                   >
